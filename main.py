33ea46da1c0b95eb08730192b19f8efed9b0ddff
VedantShirgaonkar <vedxntshirgaonkar@gmail.com>
2025-07-28 11:13:37 +0530

    data transformation pipeline setup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.p1_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.p2_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.p3_data_transformation import DataTransformationTrainingPipeline   
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -18,6 +19,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
